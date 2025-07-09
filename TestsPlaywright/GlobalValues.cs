b808940d76c58c57576be9ab9a2053b4b0a6065e
Julian Aburrow <jaburrow@btinternet.com>
2025-07-09 14:26:51 +0100

    Removed commented code in build file and commented out Manufacturer Playwright tests as they will fail until there is a database in place.

--- TestsPlaywright/GlobalValues.cs
+++ TestsPlaywright/GlobalValues.cs
@@ -2,6 +2,6 @@
 
 public static class GlobalValues
 {
-    public static string BaseUrl = "https://127.0.0.1:5245";
-    public static bool IsHeadless = true;
+    public const string BaseUrl = "https://127.0.0.1:5245";
+    public const bool IsHeadless = true;
 }
