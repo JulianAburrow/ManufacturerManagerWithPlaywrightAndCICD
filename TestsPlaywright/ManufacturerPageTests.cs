ed0c4fb8ade34f6d6d595bf89c2d4d39c85ecd94
Julian Aburrow <jaburrow@btinternet.com>
2025-07-10 12:30:12 +0100

    Added script to call Docker Container with database and uncommented tests that require a db connection

--- TestsPlaywright/ManufacturerPageTests.cs
+++ TestsPlaywright/ManufacturerPageTests.cs
@@ -1,48 +1,48 @@
 ﻿namespace TestsPlaywright;
 
-//public class ManufacturerPageTests
-//{
-//    [Fact]
-//    public async Task ManufacturerHomePageLoads()
-//    {
-//        using var playwright = await Playwright.CreateAsync();
-//        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-//        {
-//            Headless = GlobalValues.IsHeadless
-//        });
-//        await using var context = await browser.NewContextAsync(new BrowserNewContextOptions
-//        {
-//            IgnoreHTTPSErrors = true
-//        });
-//        var page = await context.NewPageAsync();
+public class ManufacturerPageTests
+{
+    [Fact]
+    public async Task ManufacturerHomePageLoads()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = GlobalValues.IsHeadless
+        });
+        await using var context = await browser.NewContextAsync(new BrowserNewContextOptions
+        {
+            IgnoreHTTPSErrors = true
+        });
+        var page = await context.NewPageAsync();
 
-//        await page.GotoAsync($"{GlobalValues.BaseUrl}/manufacturers/index", new PageGotoOptions
-//        {
-//            WaitUntil = WaitUntilState.NetworkIdle
-//        });
-//        var title = await page.TitleAsync();
+        await page.GotoAsync($"{GlobalValues.BaseUrl}/manufacturers/index", new PageGotoOptions
+        {
+            WaitUntil = WaitUntilState.NetworkIdle
+        });
+        var title = await page.TitleAsync();
 
-//        Assert.Equal("Manufacturers", title);
-//    }
+        Assert.Equal("Manufacturers", title);
+    }
 
-//    [Fact]
-//    public async Task ManufacturerCreatePageLoads()
-//    {
-//        using var playwright = await Playwright.CreateAsync();
-//        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-//        {
-//            Headless = GlobalValues.IsHeadless
-//        });
-//        await using var context = await browser.NewContextAsync(new BrowserNewContextOptions
-//        {
-//            IgnoreHTTPSErrors = true
-//        });
-//        var page = await context.NewPageAsync();
-//        await page.GotoAsync($"{GlobalValues.BaseUrl}/manufacturer/create", new PageGotoOptions
-//        {
-//            WaitUntil = WaitUntilState.NetworkIdle
-//        });
-//        var title = await page.TitleAsync();
-//        Assert.Equal("Create Manufacturer", title);
-//    }
-//}
+    [Fact]
+    public async Task ManufacturerCreatePageLoads()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = GlobalValues.IsHeadless
+        });
+        await using var context = await browser.NewContextAsync(new BrowserNewContextOptions
+        {
+            IgnoreHTTPSErrors = true
+        });
+        var page = await context.NewPageAsync();
+        await page.GotoAsync($"{GlobalValues.BaseUrl}/manufacturer/create", new PageGotoOptions
+        {
+            WaitUntil = WaitUntilState.NetworkIdle
+        });
+        var title = await page.TitleAsync();
+        Assert.Equal("Create Manufacturer", title);
+    }
+}
