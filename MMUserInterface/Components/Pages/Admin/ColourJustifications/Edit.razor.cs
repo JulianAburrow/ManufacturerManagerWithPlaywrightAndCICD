adff2812ea096a7ca7e02461f25c4c97ade455d9
Julian Aburrow <jaburrow@btinternet.com>
2025-07-17 15:48:14 +0100

    Added Colour Justification UI

---
+++ MMUserInterface/Components/Pages/Admin/ColourJustifications/Edit.razor.cs
@@ -0,0 +1,37 @@
+﻿namespace MMUserInterface.Components.Pages.Admin.ColourJustifications;
+
+public partial class Edit
+{
+    protected override async Task OnInitializedAsync()
+    {
+        ColourJustificationModel = await ColourJustificationHandler.GetColourJustificationAsync(ColourJustificationId);
+        ColourJustificationDisplayModel.Justification = ColourJustificationModel.Justification;
+        MainLayout.SetHeaderValue("Edit Colour Justification");
+    }
+
+    protected override void OnInitialized()
+    {
+        MainLayout.SetBreadcrumbs(
+        [
+            GetHomeBreadcrumbItem(),
+            GetColourJustificationHomeBreadcrumbItem(),
+            GetCustomBreadcrumbItem(EditTextForBreadcrumb),
+        ]);
+    }
+
+    private async Task UpdateColourJustification()
+    {
+        ColourJustificationModel.Justification = ColourJustificationDisplayModel.Justification;
+
+        try
+        {
+            await ColourJustificationHandler.UpdateColourJustificationAsync(ColourJustificationModel, true);
+            Snackbar.Add($"Colour Justification {ColourJustificationModel.Justification} successfully updated.", Severity.Success);
+            NavigationManager.NavigateTo("/colourjustifications/index");
+        }
+        catch
+        {
+            Snackbar.Add($"An error occurred updating Colour Justification {ColourJustificationModel.Justification}. Please try again", Severity.Error);
+        }
+    }
+}
---
+++ MMUserInterface/Components/Pages/Admin/ColourJustifications/View.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace MMUserInterface.Components.Pages.Admin.ColourJustifications;
+
+public partial class View
+{
+    protected override async Task OnInitializedAsync()
+    {
+        ColourJustificationModel = await ColourJustificationHandler.GetColourJustificationAsync(ColourJustificationId);
+        MainLayout.SetHeaderValue("View Colour Justification");
+    }
+
+    protected override void OnInitialized()
+    {
+        MainLayout.SetBreadcrumbs(
+        [
+            GetHomeBreadcrumbItem(),
+            GetColourJustificationHomeBreadcrumbItem(),
+            GetCustomBreadcrumbItem(ViewTextForBreadcrumb),
+        ]);
+    }
+}
