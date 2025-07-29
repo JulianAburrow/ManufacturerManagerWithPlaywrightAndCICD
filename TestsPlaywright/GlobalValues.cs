b01d1f7c4f0b4e4191f6e88bc2dc6b7b503fe665
Julian Aburrow <jaburrow@btinternet.com>
2025-07-29 15:39:42 +0100

    Set headless = true

--- TestsPlaywright/GlobalValues.cs
+++ TestsPlaywright/GlobalValues.cs
@@ -3,7 +3,7 @@
 public static class GlobalValues
 {
     public const string BaseUrl = "https://127.0.0.1:5245";
-    public const bool IsHeadless = false;
+    public const bool IsHeadless = true;
     public static PageGotoOptions GetPageOptions() =>
         new()
         {
